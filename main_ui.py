773a86f8862317b8ea9aa72668929aaeab7cc92d
qiuzhiqian <xia_mengliang@163.com>
2017-07-06 08:22:12 +0000

    这是一个基于python的有道词典工具

---
+++ main_ui.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import yd_dict
+import tkinter as tk
+
+top = tk.Tk()
+top.title("YDDict")
+
+YD_Dict=yd_dict.Dict_Yd()
+
+TopFrame=tk.Frame(top,width=60)
+WordEntry=tk.Entry(TopFrame,width=45)
+SearchBtn=tk.Button(TopFrame,text="Search",width=15)
+
+ResultLabel=tk.Label(top,text='Nothing',height=15,width=65,bg='#FFFFDD',justify=tk.LEFT)
+
+def BtnCallback():
+	words=WordEntry.get()
+	
+	YD_Dict.GetWordInfo(YD_Dict.GetWebString(words))
+	out_string=YD_Dict.Result_Formate()
+	ResultLabel.configure(text=out_string,anchor=tk.NW)
+
+SearchBtn.configure(command=BtnCallback)
+
+WordEntry.grid(row=0,column=0)
+SearchBtn.grid(row=0,column=1)
+
+TopFrame.grid(row=0,column=0)
+ResultLabel.grid(row=1,column=0)
+
+
+# 进入消息循环
+top.mainloop()
---
+++ yd_dict.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+#------------------------------
+#有道词典python
+#作者:秋之前
+#时间:2017.7.6
+#------------------------------
+
+import re
+import time
+import urllib.request
+import sys
+
+class Dict_Yd:
+	keyword=''
+	phonetic=[]
+	result=[]
+	
+	def __init__(self):
+		self.Trans_yd_result=r'<div id="phrsListTab" class="trans-wrapper clearfix">([\s\S]*?)<div id="webTrans" class="trans-wrapper trans-tab">'
+		
+		self.Keyword_tag=r'<h2 class="wordbook-js">([\s\S]*?)<span class="keyword">([\s\S]*?)</span>([\s\S]*?)<div class="trans-container">'
+		
+		self.Content_tag=r'<div class="trans-container">([\s\S]*?)<div id="webTrans" class="trans-wrapper trans-tab">'
+		
+	def GetWordInfo(self,webString):
+		resultString=''
+		keywordString=''
+		contentString=''
+		formate=r'\s?<.*?>'
+		
+		resultString=re.search(self.Trans_yd_result,webString).group(0)
+		
+		keywordString=re.search(self.Keyword_tag,resultString).group(0)
+		
+		dcString=re.search(r'<span class="keyword">.*?</span>',keywordString)
+		self.keyword=re.sub(formate,'',dcString[0])
+		#print(self.keyword)
+		
+		EybStringList=re.findall(r'<span class="phonetic">.*?</span>',keywordString)
+		for item in EybStringList:
+			self.phonetic.append(re.sub(formate,'',item))
+		
+		contentString=re.search(self.Content_tag,resultString).group(0)
+		
+		jsStringList=re.findall(r'<li>.*?</li>',contentString)
+		for item in jsStringList:
+			self.result.append(re.sub(formate,'',item))
+		
+	def GetWebString(self,words):
+		ydurl=r'http://www.youdao.com/w/eng/'+words+'/#keyfrom=dict2.index'
+		user_agent=r'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+		headers = {'User-Agent':user_agent}
+		req = urllib.request.Request(ydurl, headers=headers)
+		Res = urllib.request.urlopen(req).read().decode('utf-8')
+		return Res
+	
+	def Result_Formate(self):
+		f_string="%s\n" %(self.keyword)
+		if(len(self.phonetic)==2):
+			f_string=f_string+("英:%s\t美:%s" %(self.phonetic[0],self.phonetic[1]))
+		
+		if(len(self.result)):
+			f_string=f_string+"\n解释:\n"
+			for index in range(len(self.result)):
+				f_string=f_string+("\t%d: %s\n" %(index+1,self.result[index]))
+		
+		return f_string
+		
+if __name__=='__main__':
+	words=''
+	
+	if(len(sys.argv)<2):
+		words=input("请输入单词:")
+	else:
+		words=sys.argv[1]
+	
+	yds=Dict_Yd()
+	YDWebString=yds.GetWebString(words)
+	yds.GetWordInfo(YDWebString)
+	out_string=yds.Result_Formate()
+	print(out_string)
