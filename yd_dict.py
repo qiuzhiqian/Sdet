fcae37bf99cce1012772f0c1d0de1c1c66046754
qiuzhiqian <xia_mengliang@163.com>
2017-07-07 02:44:53 +0000

    添加汉译英，完善文档

--- yd_dict.py
+++ yd_dict.py
@@ -7,17 +7,32 @@ import urllib.request
 import sys
 
 class Dict_Yd:
+    type=''
     keyword=''
     phonetic=[]
     result=[]
-	
+
     def __init__(self):
         self.Trans_yd_result=r'<div id="phrsListTab" class="trans-wrapper clearfix">([\s\S]*?)<div id="webTrans" class="trans-wrapper trans-tab">'
         
         self.Keyword_tag=r'<h2 class="wordbook-js">([\s\S]*?)<span class="keyword">([\s\S]*?)</span>([\s\S]*?)<div class="trans-container">'
         
         self.Content_tag=r'<div class="trans-container">([\s\S]*?)<div id="webTrans" class="trans-wrapper trans-tab">'
-    	
+    
+    def GetWebString(self,words):
+        if (ord(list(words)[0]) not in range(97,122) and ord(list(words)[0]) not in range(65,90)):  #中转英
+            self.type='C2E'
+            words=urllib.request.quote(words)
+        else:                                                                                       #英转中
+            self.type='E2C'
+        
+        ydurl=r'http://www.youdao.com/w/eng/'+words+'/#keyfrom=dict2.index'
+        user_agent=r'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
+        headers = {'User-Agent':user_agent}
+        req = urllib.request.Request(ydurl, headers=headers)
+        Res = urllib.request.urlopen(req).read().decode('utf-8')
+        return Res
+    
     def GetWordInfo(self,webString):
         resultString=''
         keywordString=''
@@ -33,7 +48,6 @@ class Dict_Yd:
 
         dcString=re.search(r'<span class="keyword">.*?</span>',keywordString)
         self.keyword=re.sub(formate,'',dcString[0])
-        #print(self.keyword)
 
         EybStringList=re.findall(r'<span class="phonetic">.*?</span>',keywordString)
         for item in EybStringList:
@@ -41,29 +55,32 @@ class Dict_Yd:
 
         contentString=re.search(self.Content_tag,resultString).group(0)
 
-        jsStringList=re.findall(r'<li>.*?</li>',contentString)
-        for item in jsStringList:
-            self.result.append(re.sub(formate,'',item))
-    	
-    def GetWebString(self,words):
-        ydurl=r'http://www.youdao.com/w/eng/'+words+'/#keyfrom=dict2.index'
-        user_agent=r'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
-        headers = {'User-Agent':user_agent}
-        req = urllib.request.Request(ydurl, headers=headers)
-        Res = urllib.request.urlopen(req).read().decode('utf-8')
-        return Res
+        if(self.type=='E2C'):                                               #获取英转中解释列表
+            jsStringList=re.findall(r'<li>.*?</li>',contentString)
+            for item in jsStringList:
+                self.result.append(re.sub(formate,'',item))
+            
+        elif(self.type=='C2E'):                                             #获取中转英解释列表
+            jsStringList=re.findall(r'<p class="wordGroup">[\s\S]*?</span>\s*?</p>',contentString)
+            for item in jsStringList:
+                wordtypeList=re.findall(r'<span style="font-weight[\s\S]*?</span>',item)
+                wordjsList=re.findall(r'<a class="search-js" href=[\s\S]*?</a>',item)
+                for index in range(len(wordtypeList)):
+                    self.result.append(re.sub(formate,'',wordtypeList[index])+'\t'+re.sub(formate,'',wordjsList[index]))
 
     def Result_Formate(self):
         f_string="%s\n" %(self.keyword)
-        if(len(self.phonetic)==2):
+        if(len(self.phonetic)==2 and self.type=='E2C'):
             f_string=f_string+("英:%s\t美:%s" %(self.phonetic[0],self.phonetic[1]))
+        elif(len(self.phonetic)==1 and self.type=='C2E'):
+            f_string=f_string+("拼音:%s" %(self.phonetic[0]))
 
         if(len(self.result)):
             f_string=f_string+"\n解释:\n"
             for index in range(len(self.result)):
                 f_string=f_string+("\t%d: %s\n" %(index+1,self.result[index]))
         return f_string
-	
+
 if __name__=='__main__':
     words=''
     
